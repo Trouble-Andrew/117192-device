80d927e54771c4e70c1d47e1bb3fa0354b66d350
Trouble-Andrew <satyr663@mail.ru>
2018-09-07 15:17:11 +0300

    Дополняет js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,45 @@ var fieldText = popup.querySelector("#input-text");
 var storageName = localStorage.getItem("fieldName");
 var isStorageSupport = true;
 var storage = "";
+var slideMain1 = document.querySelector(".slider__slide:nth-child(1)");
+var slideMain2 = document.querySelector(".slider__slide:nth-child(2)");
+var slideMain3 = document.querySelector(".slider__slide:nth-child(3)");
+var slideMainButton = document.querySelector(".slider__radio");
+var slideMainButton1 = document.querySelector("button[name='slide-1']");
+var slideMainButton2 = document.querySelector("button[name='slide-2']");
+var slideMainButton3 = document.querySelector("button[name='slide-3']");
+
+
+// function sliderMain(num) {
+//   slideMainButton(num).addEventListener("click", function(evt) {
+//     evt.preventDefault();
+//     slideMain(x).classList.add("slider__slide--active");
+//   });
+// }
+//
+// sliderMain(3);
+
+slideMainButton3.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  slideMainButton.classList.remove("slider__radio--active");
+  slideMain1.classList.remove("slider__slide--active");
+  slideMain2.classList.remove("slider__slide--active");
+  slideMain3.classList.add("slider__slide--active");
+  slideMainButton.classList.add("slider__radio--active");
+});
+slideMainButton2.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  slideMain1.classList.remove("slider__slide--active");
+  slideMain2.classList.add("slider__slide--active");
+  slideMain3.classList.remove("slider__slide--active");
+});
+slideMainButton1.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  slideMain1.classList.add("slider__slide--active");
+  slideMain2.classList.remove("slider__slide--active");
+  slideMain3.classList.remove("slider__slide--active");
+});
+
 
 try {
   storage = localStorage.getItem(fieldName);
@@ -68,6 +107,21 @@ modalForm.addEventListener("submit", function(evt) {
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
+    if (!fieldName.value) {
+      fieldName.classList.add("modal-form__field--invalid");
+    } else {
+      fieldName.classList.remove("modal-form__field--invalid");
+    }
+    if (!fieldEmail.value) {
+      fieldEmail.classList.add("modal-form__field--invalid");
+    } else {
+      fieldEmail.classList.remove("modal-form__field--invalid");
+    }
+    if (!fieldText.value) {
+      fieldText.classList.add("modal-form__field--invalid");
+    } else {
+      fieldText.classList.remove("modal-form__field--invalid");
+    }
   } else {
     if (isStorageSupport) {
       localStorage.setItem("fieldName", fieldName.value);
