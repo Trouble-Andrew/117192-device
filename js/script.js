f127662243bc4f497dddcd689aa09e3f7d35aa6c
Trouble-Andrew <satyr663@mail.ru>
2018-09-07 12:27:46 +0300

    Добавляет js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
 var link = document.querySelector(".contacts__btn");
 var popup = document.querySelector(".modal__write-us");
 var overlay = document.querySelector(".overlay");
+var mapLink = document.querySelector(".map-link");
+var map = document.querySelector(".modal__map");
 var popupClose = document.querySelector(".btn--close");
-var fieldName = popup.querySelector("#contact-name");
 var modalForm = popup.querySelector(".modal-form");
+var fieldName = popup.querySelector("#contact-name");
 var fieldEmail = popup.querySelector("#contact-email");
 var fieldText = popup.querySelector("#input-text");
+var storageName = localStorage.getItem("fieldName");
+var isStorageSupport = true;
+var storage = "";
 
+try {
+  storage = localStorage.getItem(fieldName);
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  overlay.classList.add("modal-show");
+  if (storageName) {
+    fieldName.value = storageName;
+    fieldEmail.focus();
+  } else {
+    fieldName.focus();
+  }
+});
+
+mapLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  map.classList.add("modal-show");
   overlay.classList.add("modal-show");
   fieldName.focus();
 });
@@ -18,6 +40,7 @@ link.addEventListener("click", function(evt) {
 popupClose.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
   overlay.classList.remove("modal-show");
 });
 
@@ -25,10 +48,29 @@ overlay.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   overlay.classList.remove("modal-show");
+  map.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+      overlay.classList.remove("modal-show");
+    }
+  }
 });
 
 modalForm.addEventListener("submit", function(evt) {
   evt.preventDefault();
-  console.log(fieldName.value);
-  console.log(fieldEmail.value);
+  if (!fieldName.value || !fieldEmail.value || !fieldText.value) {
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("fieldName", fieldName.value);
+    }
+  }
 });
